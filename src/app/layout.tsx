8aef5fe8cb321f61ead7e73b61b9f816500f5ea6
Jack Burke <jburke5@sheffield.ac.uk>
2024-11-17 07:21:28 +0000

    make pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="bg-gitBackground">
+      <head>
+        <meta name='viewport' content='initial-scale=1, viewport-fit=cover'/>
+        <link rel='manifest' href="/manifest.json" />
+        <link rel="icon" href="/icons/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/icons/apple_touch_icon.png"/>
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+        <meta name="apple-mobile-web-app-title" content="growth"/>
+        <meta name="theme-color" content="#212121"/>
+      </head>
       <body className={jersey_25.className}>
         <main>
           {children}
