5f8da52b2ed98840086d177c4ce13739a7d1d6e4
Jack Sanders <personal@jacksanders.uk>
2024-11-17 06:41:31 +0000

    f

---
+++ src/app/room/[roomCode]/Lobby.tsx
@@ -0,0 +1,104 @@
+import React, {useEffect, useState} from "react";
+import {usePathname, useRouter} from "next/navigation";
+import {BarBackground} from "@/components/barBackground";
+
+export default function Lobby({
+  nextRound
+}: {
+  nextRound: () => void
+}) {
+  const [username, setUsername] = useState("");
+
+  const roomCode = usePathname().replace("/room/", ""); // remove room/... from the path
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedUsername = sessionStorage.getItem("username");
+
+      if (storedUsername) {
+        setUsername(storedUsername);
+      } else {
+        router.push("/enter-name");
+      }
+    }
+  }, [router]);
+
+  return (
+    <>
+      <div className="relative min-h-screen overflow-hidden">
+        <BarBackground />
+        <div className="w-full bg-black p-5 bg-opacity-50 flex flex-row items-center justify-between">
+          <h1 className="text-4xl text-white text-shadow-effect">room id: {roomCode}</h1>
+          <button
+            onClick={nextRound}
+            className="bg-gray-900 text-white text-4xl px-4 py-2"
+          >
+            Start
+          </button>
+        </div>
+        <div className="p-5 pb-0">
+          <h1 className="text-4xl text-white text-shadow-effect">meet your judges</h1>
+          <div className="flex flex-row space-x-5">
+
+            <div className="judge-box flex flex-col items-center justify-between text-center max-w-[200px] flex-grow">
+              <img className="h-[200px] max-w-full object-contain" src="/characters/countess.png" />
+              <p className="text-white text-shadow-effect text-3xl">Countess Crookes</p>
+              <p className="text-white text-shadow-effect text-lg break-words px-2 min-h-[85px] hidden sm:block">
+                Professional and successful entrepreneur with expert knowledge
+              </p>
+            </div>
+            <div className="judge-box flex flex-col items-center justify-between text-center max-w-[200px] flex-grow">
+              <img className="h-[200px] max-w-full object-contain" src="/characters/sue.png" />
+              <p className="text-white text-shadow-effect text-3xl">Sue Stainability</p>
+              <p className="text-white text-shadow-effect text-lg break-words px-2 min-h-[85px] hidden sm:block">
+                Cares about sustainability and inclusivity in business solutions
+              </p>
+            </div>
+            <div className="judge-box flex flex-col items-center justify-between text-center max-w-[200px] flex-grow">
+              <img className="h-[200px] max-w-full object-contain" src="/characters/dave.png" />
+              <p className="text-white text-shadow-effect text-3xl">Dave O' Pub</p>
+              <p className="text-white text-shadow-effect text-lg break-words px-2 min-h-[85px] hidden sm:block">
+                Always drunk, enjoys the pub, and gives honest opinions on business ideas
+              </p>
+            </div>
+            <div className="judge-box flex flex-col items-center justify-between text-center max-w-[200px] flex-grow">
+              <img className="h-[200px] max-w-full object-contain" src="/characters/child.png" />
+              <p className="text-white text-shadow-effect text-3xl">Tiny Tim</p>
+              <p className="text-white text-shadow-effect text-lg break-words px-2 min-h-[85px] hidden sm:block">
+                Very naive and enjoys humour in business solutions
+              </p>
+            </div>
+
+          </div>
+        </div>
+        <div className="p-5">
+          <h1 className="text-4xl text-white text-shadow-effect">players (0)</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center">
+            <div className="text-white text-shadow-effect text-3xl flex items-center justify-center">
+              <img className="h-[20px] max-w-full object-contain mr-2" src="/conn_status.png" />
+              jackb
+            </div>
+            <div className="text-white text-shadow-effect text-3xl flex items-center justify-center">
+              <img className="h-[20px] max-w-full object-contain mr-2" src="/conn_status.png" />
+              jackb
+            </div>
+            <div className="text-white text-shadow-effect text-3xl flex items-center justify-center">
+              <img className="h-[20px] max-w-full object-contain mr-2" src="/conn_status.png" />
+              jackb
+            </div>
+            <div className="text-white text-shadow-effect text-3xl flex items-center justify-center">
+              <img className="h-[20px] max-w-full object-contain mr-2" src="/conn_status.png" />
+              jackb
+            </div>
+            <div className="text-white text-shadow-effect text-3xl flex items-center justify-center">
+              <img className="h-[20px] max-w-full object-contain mr-2" src="/conn_status.png" />
+              jackb
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/room/[roomCode]/page.tsx
+++ src/app/room/[roomCode]/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 import { usePathname, useRouter } from "next/navigation";
-import { BarBackground } from "@/components/barBackground";
 import { pusherClient } from "@/lib/pusher";
 import {Channel} from "pusher-js";
-import { Judge } from "@/components/Judge";
+import QuestionTime from "@/app/room/[roomCode]/QuestionTime";
+import Lobby from "@/app/room/[roomCode]/Lobby";
+import Leaderboard from "@/app/room/[roomCode]/Leaderboard";
 
 // Define the shape of the Member object
 interface Member {
@@ -31,28 +31,18 @@ const RoomPage: React.FC = () => {
   const [roomReady, setRoomReady] = useState<boolean>(false);
   const [username, setUsername] = useState<string>();
   const [channel, setChannel] = useState<Channel>();
+  const [round, setRound] = useState(0);
+  const [score, setScore] = useState(0);
 
   const roomCode = usePathname().replace("/room/", ""); // Extract roomCode from the path
-  const router = useRouter();
 
-  const startListener = () => {
-    if (!username || !channel) {
-      return;
-    }
-
-    fetch(`/api/ready-up?username=${username}&roomCode=presence-${roomCode}`, {
-      method: "POST",
-    }).then(r => {
-      if (r.status !== 200) {
-        throw new Error("Could not ready up :(");
+  const advanceRound = () => {
+    fetch(`/api/advance-round?roomCode=presence-${roomCode}`).then(res => {
+      if (res.status !== 200){
+        throw new Error("no")
       }
-
-      return r.json();
-    }).then(data => {
-      setReady(data);
-      channel.trigger('client-member_ready', { memberUsername: username, readyStatus: data });
-      members.find((member) => member.username === username)!.ready = data;
-    });
+      return res.text();
+    }).then(data => setRound(parseInt(data)));
   }
 
   useEffect(() => {
@@ -123,40 +113,31 @@ const RoomPage: React.FC = () => {
 
   return (
     <>
-      <div className="relative min-h-screen overflow-hidden">
-        <BarBackground />
-        <div className="w-full bg-black p-5 bg-opacity-50 flex flex-row items-center justify-between">
-          <h1 className="text-4xl text-white text-shadow-effect">room id: {roomCode}</h1>
-          <button 
-            onClick={startListener}
-            className="bg-gray-900 text-white text-4xl px-4 py-2"
-          >
-            {ready ? "Un-ready" : "Ready Up!"}
-          </button>
-        </div>
-        <div className="p-5 pb-0">
-          <h1 className="text-4xl text-white text-shadow-effect">meet your judges</h1>
-          <div className="flex flex-row space-x-5">
-            <Judge name="Countess Crookes" description="Professional and successful entrepreneur with expert knowledge" imgLink="countess.png" />
-            <Judge name="Sue Stainability" description="Cares about sustainability and inclusivity in business solutions" imgLink="sustainability.png" />
-            <Judge name="Dave O' Pub" description="Always drunk, enjoys the pub, and gives honest opinions on business ideas" imgLink="dave.png" />
-            <Judge name="Tiny Tim" description="Very naive and enjoys humour in business solutions" imgLink="child.png" />
-          </div>
-        </div>
-        <div className="p-5">
-          <h1 className="text-4xl text-white text-shadow-effect">players ({members.length})</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center">
-            { members.map((member) => (
-              <div key={member.username} className="text-white text-shadow-effect text-3xl flex items-center justify-center">
-                <img className="h-[20px] max-w-full object-contain mr-2" src="/conn_status.png" alt="good connection"/>
-                {member.username} {member.ready ? " (Ready)" : ""}
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      {(round === 0) && (
+        <Lobby
+          nextRound={advanceRound}
+        />
+      )}
+
+      {(round > 0 && round < 6) && (
+        <QuestionTime
+          score={score}
+          addScore={(newScore: number) => setScore(score + newScore)}
+          roundNumber={round}
+          nextRound={advanceRound}
+        />
+      )}
+
+      {(round < 0) && (
+        <Leaderboard
+          players={[{
+            username: "You!",
+            score: score,
+          }]}
+        />
+      )}
     </>
-  );
+  )
 };
 
 export default RoomPage;
