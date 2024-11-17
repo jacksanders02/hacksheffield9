ce4390185113eb1de0c1f1be61e4dec631a124c5
Jack Sanders <personal@jacksanders.uk>
2024-11-17 02:52:32 +0000

    fuck you pusher

--- src/app/api/pusher/auth/route.ts
+++ src/app/api/pusher/auth/route.ts
@@ -14,7 +14,6 @@ export async function POST(req: Request) {
   }
   const qString = chunks.join('');
 
-  console.log(qString);
   try {
     // Parse the request body to get the required fields
     // example qString: socket_id=200122.60896&channel_name=presence-12345
@@ -22,18 +21,32 @@ export async function POST(req: Request) {
     const socket_id = params.get('socket_id');
     const channel_name = params.get('channel_name');
 
-    console.log(socket_id, channel_name);
-
     // Check if the socket_id and channel_name are present
     if (!socket_id || !channel_name) {
       return new NextResponse('Missing parameters', { status: 400 });
     }
 
     // You might want to get additional user info here
+    const url = `${process.env.PYTHON_BACKEND_URL}/room/${channel_name}/user/last`;
+    let username = ""
+    await fetch(url, {
+      method: "GET",
+    }).then(res => {
+      if (res.status !== 200) {
+        throw new Error("Failed to join");
+      } else {
+        return res.json();
+      }
+    }).then((data) => {
+      username = data;
+    })
+
     const user = {
-      id: "user123", // Unique user ID
-      username: "username123", // You can use session or any other way to retrieve this
+      id: username, // Unique user ID
+      username, // You can use session or any other way to retrieve this
     };
+
+    console.log(user);
 
     // Authenticate with Pusher using the socket_id and channel_name
     const authResponse = pusherServer.authenticate(socket_id, channel_name, {
